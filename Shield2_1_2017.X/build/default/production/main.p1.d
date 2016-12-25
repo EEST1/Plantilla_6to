73be4c006b26b86bc1388e4b88671d47cd1a323a
Esteban <estebanlem@gmail.com>
2016-12-25 17:54:56 -0300

    Plantilla Ordenada para trabajo en LCD / USART
    La plantilla está lista para el trabajo con ambas placas adicionales denominadas Shield1_3 y Shield 2_1

---
+++ Shield2_1_2017.X/build/default/production/main.p1.d
@@ -0,0 +1,7 @@
+ build/default/production/main.d  \
+ build/default/production/main.p1:  \
+ main.c  \
+ConfShield2_1.h  \
+lcd.h  \
+confbits.h  \
+Tecnica1.h 
---
+++ Shield2_1_2017.X/main.c
@@ -0,0 +1,45 @@
+/****************************************************************************
+ * File:   main.c                                                           *
+ * Author: Esteban Lemos                                                    *
+ * Created on 5 de febrero de 2014, 20:49                                   *
+ * En caso de necesitar el proyecto completo ingresar en                    *
+ * https://github.com/EEST1/Ap_template                                     *
+ ****************************************************************************/
+
+#include <xc.h>
+#include "confbits.h"       //no se debe de modificar este archivo para
+                            //garantizar el funcionamiento del BOOTLOADER
+
+
+
+#include "Tecnica1.h"       //segun el shield habilita distintas librerias
+
+
+
+/*variables globales ********************************************************/
+
+
+void main(void) {
+    
+    demo_shield2_1_INI();
+    
+    while(1){
+        demo_shield2_1();
+
+    
+    }    
+}
+
+void __interrupt myISR(void){
+  
+    if(TMR0IF){
+        INTCONbits.TMR0IF = 0;  //borra el flag de la interrupción de timer
+        TMR0L = 209;            //reinicia el timer
+        TMR0H = 0xFF;
+        
+   
+        /*Llamada a funciones TIC 1ms ********************/
+        tic_demo_shield2_1();
+    }
+    
+}
